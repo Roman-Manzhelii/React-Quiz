609baeeb0daa90fe2ac1e2c67015dd1848999027
LiverPork <D00264678@student.dkit.ie>
2024-06-14 19:11:53 +0300

    core components for quiz

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import ImageLogo from '../assets/quiz-logo.png'
+export default function Header() {
+    return (
+        <header>
+            <img src={ImageLogo} alt='Quiz logo'/>
+            <h1>ReactQuiz</h1>
+        </header>
+
+    )
+}
